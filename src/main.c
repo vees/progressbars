3a64ab65b30f69c89f5030d3c2abb93a1c3d2048
Rob Carlson <rob@vees.net>
2014-10-26 10:53:26 -0400

    Added test background on load

--- src/main.c
+++ src/main.c
@@ -38,12 +38,59 @@ static void minute_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   // APP_LOG(APP_LOG_LEVEL_DEBUG, "PEBBLE: Tick");
   // time_t now = time(NULL);
   //strftime(time_buffer, sizeof("00:00"), "%H:%M", tick_time);
-  snprintf(time_buffer, 7, latitude);
-  text_layer_set_text(text_layer, time_buffer);
+  //snprintf(time_buffer, 7, latitude);
+  //text_layer_set_text(text_layer, time_buffer);
 }
 
+static Layer *background_layer;
+static BitmapLayer *b_clockface_layer, *w_clockface_layer;
+static GBitmap *b_clockface_image, *w_clockface_image;
+
+static void save_data() {
+  /* Save data to persistent storage if we have it.*/
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  // Create background clock including the daylight path.
+  background_layer = layer_create(bounds);
+  layer_add_child(window_layer, background_layer);
+
+  w_clockface_image = gbitmap_create_with_resource(RESOURCE_ID_CLOCKFACE);
+  w_clockface_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(w_clockface_layer, w_clockface_image);
+  bitmap_layer_set_background_color(w_clockface_layer, GColorClear);
+  bitmap_layer_set_compositing_mode(w_clockface_layer, GCompOpAssign);
+  layer_add_child(background_layer, bitmap_layer_get_layer(w_clockface_layer));
+}
+
+static void window_unload(Window *window) {
+  // Save data to persistent storage
+  save_data();
+
+  // Destroy TextLayers.
+
+  // Destroy GBitmaps.
+  gbitmap_destroy(w_clockface_image);
+
+  // Destroy BitmapLayrs.
+  bitmap_layer_destroy(w_clockface_layer);
+
+  // Destroy Layers.
+}
+
+
 static void init() {
+  // Initialize window.
   window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+    
+  //window = window_create();
   window_stack_push(window, true /* Animated */);
   
   // Message registration
@@ -52,19 +99,7 @@ static void init() {
   app_message_register_outbox_failed(out_failed_handler);
   app_message_register_outbox_sent(out_sent_handler);
   app_message_open(256, 256);
-  
-  latitude = "0"; 
-  longitude = "0";
-
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_frame(window_layer);
-  text_layer = text_layer_create((GRect){ .origin = { 0, 30 }, .size = bounds.size });
-  text_layer_set_text(text_layer, clock_is_24h_style() ? "Mode:\n24" : "Mode:\n12");
-  // Font reference at https://developer.getpebble.com/blog/2013/07/24/Using-Pebble-System-Fonts/
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  
+    
   tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) minute_tick_handler);
   //tick_timer_service_subscribe(SECOND_UNIT, (TickHandler) second_tick_handler);
 }
